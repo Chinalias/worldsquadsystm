9ef57d1e3e2c67d910a52d09f5e783b83b7ee6db
laimamssd <35742714+laimamssd@users.noreply.github.com>
2018-11-03 21:58:23 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@ const client = new Discord.Client();
 const prefix = "*";
 const config = require("./config.json")
 
-const devs = ["331081268731052042,376480203389665281"]
+const devs = ["331081268731052042"]
 
 const adminprefix = "*";
 client.on('message', message => {
